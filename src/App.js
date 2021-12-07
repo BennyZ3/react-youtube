1d0d42cd59a06db1e960b3cf3d2c7a06579b3cb2
DylanLovell <arcanyne7@gmail.com>
2021-12-07 13:56:48 -0500

    functional search bar

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import { Component } from "react";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Nav from "./Components/Nav";
-import Result from "./Components/Result";
-import About from "./Components/About";
-import Search from "./Components/Search";
-import Video from "./Components/Video";
+import { Component } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Nav from './Components/Nav';
+import Result from './Components/Result';
+import About from './Components/About';
+import Search from './Components/Search';
+import Video from './Components/Video';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: "",
-      DataIsLoaded: false,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     data: '',
+  //     DataIsLoaded: false,
+  //   };
+  // }
 
-  componentDidMount() {
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=ariana+grande
-      &key=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          data: json,
-          DataIsLoaded: true,
-        });
-      });
-  }
+  // getData() {
+  //   fetch(
+  //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=ariana+grande
+  //     &key=${process.env.REACT_APP_API_KEY}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       this.setState({
+  //         data: json,
+  //         DataIsLoaded: true,
+  //       });
+  //     });
+  // }
 
   render() {
-    const { DataIsLoaded, data } = this.state;
+    // const { DataIsLoaded, data } = this.state;
     // console.log(data);
 
-    if (!DataIsLoaded)
-      return (
-        <div>
-          <h1> Pleases wait some time.... </h1>{" "}
-        </div>
-      );
+    // if (!DataIsLoaded)
+    //   return (
+    //     <div>
+    //       <h1> Pleases wait some time.... </h1>{' '}
+    //     </div>
+    //   );
 
     return (
       <div className="App">
@@ -48,7 +48,7 @@ class App extends Component {
 
         <main>
           <Routes>
-            <Route path="/videos" element={<Result data={data} />} />
+            <Route path="/videos?search_query=:search" element={<Result />} />
             <Route path="/videos/:id" element={<Video />} />
             <Route path="/about" element={<About />} />
           </Routes>
@@ -59,4 +59,3 @@ class App extends Component {
 }
 
 export default App;
-// key=AIzaSyAxV3nD9zKlpQ720FWFQaNnK3mynXp6W0Q`
