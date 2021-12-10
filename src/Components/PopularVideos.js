a1fafab4ac016787436ebeac75325c5b0b71c529
DylanLovell <arcanyne7@gmail.com>
2021-12-10 17:30:28 -0500

    edge case handling for error in api

--- src/Components/PopularVideos.js
+++ src/Components/PopularVideos.js
@@ -1,12 +1,12 @@
-import "./Result.css";
-import { Component, useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import YouTube from "react-youtube";
-import ReactPlayer from "react-player";
-import ResultVideo from "./ResultVideo";
+import './Result.css';
+import { Component, useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import YouTube from 'react-youtube';
+import ReactPlayer from 'react-player';
+import ResultVideo from './ResultVideo';
 
 const PopularVideo = () => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState('');
   const [play, setPlay] = useState(false);
 
   let result = null;
@@ -20,15 +20,17 @@ const PopularVideo = () => {
         setData(json);
       })
       .catch((error) => {
-        console.log("error");
+        console.log('error');
       });
   }, []);
 
   if (data) {
-    result = data.items.map((element) => <ResultVideo vid={element} />);
+    result = !data.error
+      ? data.items.map((element) => <ResultVideo vid={element} />)
+      : data.error.errors.map((error) => <p>{error.message}</p>);
   }
 
-  return <div className="Result">{result ? result : "Loading"}</div>;
+  return <div className="Result">{result ? result : 'Loading'}</div>;
 };
 
 export default PopularVideo;
--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,23 +1,20 @@
-import "./Result.css";
-import { Component, useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import YouTube from "react-youtube";
-import ReactPlayer from "react-player";
-import ResultVideo from "./ResultVideo";
+import './Result.css';
+import { Component, useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import YouTube from 'react-youtube';
+import ReactPlayer from 'react-player';
+import ResultVideo from './ResultVideo';
 
 const Result = () => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState('');
   const [play, setPlay] = useState(false);
   const { search } = useParams();
-
 
   let result = null;
 
   useEffect(() => {
     fetch(
-
       `https://youtube.googleapis.com/youtube/v3/search?maxResults=6&part=snippet&q=${search}&key=${process.env.REACT_APP_API_KEY}`
-
     )
       .then((res) => res.json())
       .then((json) => {
@@ -29,9 +26,11 @@ const Result = () => {
   }, [search]);
 
   if (data) {
-    result = data.items.map((element) => <ResultVideo vid={element} />);
+    result = !data.error
+      ? data.items.map((element) => <ResultVideo vid={element} />)
+      : data.error.errors.map((error) => <p>{error.message}</p>);
   }
-  return <div className="Result">{result ? result : "Loading"}</div>;
+  return <div className="Result">{result ? result : 'Loading'}</div>;
 };
 
 export default Result;
