10fc9aaf275c90b1d10335c4a2d1ec05137e7643
BennyZ3 <bennyz3@hotmail.com>
2021-12-12 12:57:37 -0500

    simple about card layout

---
+++ src/Components/AboutCard.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+
+class AboutCard extends Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    console.log(this.props.collaborators[0]);
+    let aboutCards = this.props.collaborators.map((person) => {
+      let { firstName, lastName, github, linkedIn, bio } = person;
+      return (
+        <div className="aboutCard">
+          <h2>{firstName + " " + lastName}</h2>
+          <p>
+            <strong>GitHub: </strong>
+            <a href={github} target="_blank">
+              {github}
+            </a>
+          </p>
+          <p>
+            <strong>LinkedIn: </strong>
+            <a href={linkedIn} target="_blank">
+              {linkedIn}
+            </a>
+          </p>
+          <p>
+            <strong>Bio: </strong>
+            {bio}
+          </p>
+        </div>
+      );
+    });
+    return <>{aboutCards}</>;
+  }
+}
+
+export default AboutCard;
