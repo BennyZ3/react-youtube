ccc1e137d6fc7b332d6b0396452a3c3497a4bec7
BennyZ3 <bennyz3@hotmail.com>
2021-12-12 12:56:34 -0500

    removed excess commented code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Component } from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Nav from './Components/Nav';
-import Result from './Components/Result';
-import About from './Components/About';
-import Video from './Components/Video';
-import PopularVideo from './Components/PopularVideos';
+import { Component } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Result from "./Components/Result";
+import About from "./Components/About";
+import Video from "./Components/Video";
+import PopularVideo from "./Components/PopularVideos";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: '',
+      data: "",
     };
   }
 
@@ -22,49 +22,19 @@ class App extends Component {
     )
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
+        // console.log(json);
         this.setState({
           data: json,
           DataIsLoaded: true,
         });
       });
   }
-  // getData() {
-  //   fetch(
-  //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=ariana+grande
-  //     &key=${process.env.REACT_APP_API_KEY}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       this.setState({
-  //         data: json,
-  //         DataIsLoaded: true,
-  //       });
-  //     });
-  // }
-
-  // updateData = (newData) => {
-  //   this.setState({
-  //     data: newData,
-  //   });
-  // };
 
   render() {
-    // const { DataIsLoaded, data } = this.state;
-    // console.log(data);
-
-    // if (!DataIsLoaded)
-    //   return (
-    //     <div>
-    //       <h1> Pleases wait some time.... </h1>{' '}
-    //     </div>
-    //   );
-
     const { data } = this.state;
     return (
       <div className="App">
         <Nav />
-
         <main>
           <Routes>
             <Route exact path="/" element={<PopularVideo />} />
